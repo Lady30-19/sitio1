bc82ac72a13f2d2f7b9449c15c148941934a5905
Lady30-19 <laura.apaza1430@gmail.com>
2023-06-25 10:53:38 -0500

    mipagina

--- unid1/s06_evaluacion1/js/form01validar(NO).js
+++
@@ -1,9 +0,0 @@
-function fnValidar01()
-{
-    let xNombre = document.getElementById("tres").value;
-    if(xNombre=="" ||/^\s+$/.test(xNombre)) {
-        alert("No ingresar espacios");
-        return false;
-   
-    }
-}
